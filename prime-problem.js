432d846f06b4fc127c78ca72b18db57d80c60ba0
Prabhat Kumar <prabhssgg3@gmail.com>
2022-11-06 21:07:17 +0530

    member 1 has solved the prime problem

---
+++ prime-problem.js
@@ -0,0 +1,20 @@
+function checkPrime(n){
+    let isPrime = true;
+    for(let i = 2; i*i <= n; i++){
+        if(n % i != 0){
+            isPrime = false;
+            break;
+        }
+    }
+
+    return isPrime;
+}
+
+let num = 11;
+
+if(checkPrime(num)){
+    console.log(num + " is prime");
+}
+else{
+    console.log(num + " is not prime");
+}
